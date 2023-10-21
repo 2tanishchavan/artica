52b0d8be7914a7e4b9cf7eb6213a17ae8464b0c9
prasadsawant7 <prasadsawant7git@gmail.com>
2023-10-22 02:29:24 +0530

    refactored project structure

--- server/supabase_db.py
+++ server/supabase_db.py
@@ -4,8 +4,8 @@ from supabase import create_client, Client
 
 load_dotenv()
 
-url: str = os.environ.get("VITE_SUPABASE_PROJECT_URL")
-key: str = os.environ.get("VITE_SUPABASE_PROJECT_API_KEY")
+url: str = os.environ.get("SUPABASE_PROJECT_URL")
+key: str = os.environ.get("SUPABASE_PROJECT_API_KEY")
 
 supabase: Client = create_client(url, key)
 
